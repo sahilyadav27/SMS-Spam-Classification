b2c3e6bdf1e11ecae5aac7f7f60c91e0f7043f3f
Sahil Yadav <53268444+sahilyadav27@users.noreply.github.com>
2019-08-07 21:39:28 +0530

    Update UCI.py

--- UCI.py
+++ UCI.py
@@ -4,14 +4,30 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
 
-# To download the dataset, follow the link :
+# To download the UCI dataset, follow the link :
 #     https://archive.ics.uci.edu/ml/machine-learning-databases/00228/
 
 #UCI Dataset
-df = pd.read_table('/home/shlydv/Downloads/train', sep="\t", names=['label', 'sms'])
+dfu = pd.read_table('/home/shlydv/Downloads/train', sep="\t", names=['label', 'sms'])
 
-df['label'] = df.label.map({'ham':0, 'spam':1})
+dfu['label'] = dfu.label.map({'ham':0, 'spam':1})
 
+
+# To download the IIITD dataset, follow the link :
+#     http://precog.iiitd.edu.in/requester.php?dataset=smsspam
+
+#IIITD Dataset
+dfh = pd.read_table('/home/shlydv/Downloads/ham.txt', names = ['sms'], encoding = "ISO-8859-1")
+dfs = pd.read_table('/home/shlydv/Downloads/spam.txt', names = ['sms'], encoding = "ISO-8859-1")
+
+dfh['label'] = 0
+dfs['label'] = 1
+# concatenating ham and spam
+dfd = dfh.append(dfs, ignore_index=True)
+# concatenating UCI and IIITD datasets
+df = dfd.append(dfu, ignore_index=True)
+
+# Train and Test Split
 X_train, X_test, y_train, y_test = train_test_split(df['sms'], df['label'], random_state=1)
 
 count_vector = CountVectorizer()
